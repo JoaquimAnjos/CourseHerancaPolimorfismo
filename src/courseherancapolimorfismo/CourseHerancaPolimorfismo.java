d346f741233e0e18f5162716c715d21fa1ad361a
JoaquimAnjos <quinzinhodosanjos@gmail.com>
2019-04-28 18:35:20 +0100

    testar stash

--- src/courseherancapolimorfismo/CourseHerancaPolimorfismo.java
+++ src/courseherancapolimorfismo/CourseHerancaPolimorfismo.java
@@ -21,6 +21,8 @@ public class CourseHerancaPolimorfismo {
         
         BusinessAccount bcc = new BusinessAccount(1002,"Maria",0.0, 500.0);
         Account acc = new Account(1001,"Alex",0.0);
+        
+        Account acc1 = bcc;
     }
     
 }
